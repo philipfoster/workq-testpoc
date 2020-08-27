b1a422f2139e5c03f7b88f5aba45ae0565854c46
Philip <pfoster@redhat.com>
2020-08-27 15:12:28 -0500

    fix?

--- src/main/java/com/workq/testpoc/SSNVerificationData.java
+++ src/main/java/com/workq/testpoc/SSNVerificationData.java
@@ -93,17 +93,5 @@ public class SSNVerificationData implements java.io.Serializable {
 		this.ssnImageId = ssnImageId;
 	}
 
-    @Override
-    public String toString() {
-        return "SSNVerificationData{" +
-            "id=" + id +
-            ", caseId='" + caseId + '\'' +
-            ", approved=" + approved +
-            ", automated=" + automated +
-            ", ssn='" + ssn + '\'' +
-            ", ssnImageId='" + ssnImageId + '\'' +
-            '}';
-    }
-
 
 }
